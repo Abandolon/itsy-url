677e6761d49a52a024e57163f74a3ddc199177b1
Anthony Bandolon <abandolon@gmail.com>
2019-08-10 09:28:59 +0200

    Updated _colors.scss & bootstrap vars. Also started routing configuration

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :user do
+    resource :url, only: [:index, :edit, :destroy]
+  end
+  get '/:id', to: 'url#find'
 end
